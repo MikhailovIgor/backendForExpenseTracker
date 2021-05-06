cb5469efefe2a5805651608b180f97b8667740ee
MikhailovIgor <iomihailov@gmail.com>
2021-05-06 10:33:54 +0300

    added a couple of dependencies

--- models/fuel.js
+++ models/fuel.js
@@ -6,8 +6,8 @@ const fuelSchema = new Schema({
   date: { type: Date, default: Date.now },
   money: { type: Number, required: true },
   description: String,
-  // user: { type: mongoose.Types.ObjectId, required: true },
-  user: { type: String, required: true },
+  user: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
+  // user: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Fuel', fuelSchema);
